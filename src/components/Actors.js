9786c5e3b4301466ee33181320d1c87980247d4e
Bobby Hinton <bob.g.hint@gmail.com>
2022-08-21 20:32:55 -0500

    commit tree 2419a3a4735faee987aa69c3c1018b1a12b60d7a

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return ( 
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map(value => 
+        <div key={value.name}>
+          <p>{value.name}</p>
+          <ul>
+          {value.movies.map(value => <li key={value}>{value}</li>)}
+          </ul>
+        </div>
+        )}
+    </div> 
+  )
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return ( 
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(value => 
+        <div key={value.title}>
+          <p>Title: {value.title}</p>
+          <p>Time: {value.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {value.genres.map(value => <li key={value}>{value}</li>)}
+          </ul>
+        </div>)}
+    </div> 
+  )
 }
 
 export default Movies;
